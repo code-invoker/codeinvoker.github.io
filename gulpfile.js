773384863d9c7820f7c02d3109f3a3c2240bf966
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-09-07 00:13:30 +0500

    compile assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,50 @@
-'use strict';
-
 const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+const webpack = require('webpack-stream');
+const browserSync = require('browser-sync');
 
-gulp.task('browser-sync', () => {
-  browserSync.init({
-    proxy: '127.0.0.1:4000',
-    open: false,
-    notify: false,
-    reloadOnRestart: true,
-    files: [ './dist' ]
+gulp.task('styles', () => {
+  return gulp.src('./src/assets/styles/app.css')
+    .pipe(postcss())
+    .pipe(gulp.dest('./src/public'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('scripts', () => {
+  return gulp.src('./src/assets/scripts/app.js')
+    .pipe(webpack({
+      mode: 'development',
+      devtool: 'inline-source-map',
+      output: {
+        filename: 'app.js'
+      },
+      module: {
+        rules: [{
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        }]
+      }
+    }))
+    .pipe(gulp.dest('./src/public'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('serve', () => {
+  return browserSync({
+    server: {
+      baseDir: 'dist'
+    }
   });
 });
 
-gulp.task('watch', [ 'browser-sync' ], () => {
-  gulp.watch('./dist').on('change', (e) => {
-    return gulp.src(e.path)
-      .pipe(browserSync.reload());
+gulp.task('watch', gulp.parallel('styles', 'scripts', 'serve', () => {
+  gulp.watch([ './dist/**/*.html', '!./public' ]).on('change', (path) => {
+    return gulp.src(path)
+      .pipe(browserSync.reload({ stream: true }));
   });
-});
 
-gulp.task('default', [ 'watch' ]);
+  gulp.watch('./src/assets/styles/**/*.css', gulp.series('styles'));
+  gulp.watch('./src/assets/scripts/**/*.js', gulp.series('scripts'));
+}));
+
+gulp.task('default', gulp.series('watch' ));
